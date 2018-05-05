60755233eb56d1123a89b5b37471c82e14aa8a3f
Tetsuro Yoshikawa <tetsuro.yoshikawa@seezoo.co.jp>
2018-05-05 21:21:58 +0900

    refactor: create room create button component

---
+++ src/sample-chat/components/RoomCreateButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="isCreatable">
+    <button @click="createRoom">Create Room!</button>
+  </div>
+</template>
+
+
+<script>
+import RoomStore from '~/store/RoomStore';
+
+export default {
+  props: ['isCreatable'],
+  methods:{
+    createRoom: () => {
+      RoomStore().dispatch('create');
+    },
+  },
+}
+</script>
